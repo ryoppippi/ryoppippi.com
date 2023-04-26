8fb98a42ce5f3348c3a2cfe5c994b32d44a0101a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-04-26 18:36:41 +0100

    add favicon and create my new LP

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container mx-auto flex h-full items-center justify-center">
+	<div class="space-y-5">
+		<h1>Hello World</h1>
+		<p>@ryoppippi</p>
+	</div>
+</div>
