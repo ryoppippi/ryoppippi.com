e426746d63af28c3efa9dacdb1416e777643e3d4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-21 18:07:08 +0100

    fix font color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	<div class="mt-8">
 		<h1 class="mb-6 text-center text-4xl font-bold text-gray-800">
 			<span class="block text-me-text-100 xl:inline">@ryoppippi</span>
-			<span class="text-center text-xl font-medium text-gray-500">Engineer</span>
+			<span class="text-center text-xl font-medium text-me-text-200">Engineer</span>
 		</h1>
 	</div>
 </article>
