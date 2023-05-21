26cb5e8d262125e0b625e898ffaa8db85072c920
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-21 18:43:10 +0100

    add twitter meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,19 +14,46 @@
 	import favicon32 from '$lib/assets/favicons/favicon-32x32.png';
 	import favicon16 from '$lib/assets/favicons/favicon-16x16.png';
 	import siteWebmanifest from '$lib/assets/favicons/site.webmanifest';
+	import ryoppippi from '$lib/assets/ryoppippi.jpg';
 
 	preparePageTransition();
 </script>
 
-<svelte:head>
-	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
-	<link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
-	<link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
-	<link rel="manifest" href={siteWebmanifest} />
-</svelte:head>
 <MetaTags
 	title="ryoppippi.com"
-	description="Portfolio of ryoppippi"
+	description="Portfolio of @ryoppippi"
+	twitter={{
+		handle: '@ryoppippi',
+		site: '@ryoppippi',
+		cardType: 'summary_large_image',
+		title: 'ryoppippi.com',
+		description: 'Portfolio of @ryoppippi',
+		image: ryoppippi,
+		imageAlt: "ryoppippi's icon"
+	}}
+	additionalLinkTags={[
+		{
+			rel: 'apple-touch-icon',
+			sizes: '180x180',
+			href: appleTouchIcon
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '32x32',
+			href: favicon32
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '16x16',
+			href: favicon16
+		},
+		{
+			rel: 'manifest',
+			href: siteWebmanifest
+		}
+	]}
 	robotsProps={{
 		noarchive: true,
 		nosnippet: true,
