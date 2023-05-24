f4accdde5303cfd68abf5a34388e15695bf2e02f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-24 11:25:06 +0100

    fix tag

--- src/routes/me/ChipList.svelte
+++ src/routes/me/ChipList.svelte
@@ -1,26 +1,21 @@
 <script>
 	/** @type{Record<string, boolean>} */
-	export let items = {
-		Expert: true,
-		Middle: true,
-		'Good w/ ChatGPT': true
-	};
+	export let items = {};
 
 	import IconCheck from '~icons/mdi/check';
 </script>
 
 <div class="justify-begin mb-5 flex select-none space-x-2">
 	{#each Object.keys(items) as i}
-		<span
-			class="chip {items[i] ? 'variant-filled' : 'variant-glass-soft'}"
-			on:click={() => {
-				items[i] = !items[i];
-			}}
-			on:keypress>
-			<span class="">{i}</span>
-			{#if items[i]}
-				<span><IconCheck /></span>
-			{/if}
-		</span>
+		{@const id = `cb-${i}`}
+		<input type="checkbox" class="hidden" {id} bind:checked={items[i]} />
+		<label for={id} class="chip {items[i] ? 'variant-filled' : 'variant-glass-soft'}" on:keypress>
+			<span class="flex"
+				>{i}
+				{#if items[i]}
+					<span class="peer-checked/{id}:hidden inline"><IconCheck /></span>
+				{/if}
+			</span>
+		</label>
 	{/each}
 </div>
