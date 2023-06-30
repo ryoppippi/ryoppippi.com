ebc9e3519ace557b60302f4ff23c9becd26027d7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-06-30 11:57:49 +0100

    update experiences

--- src/routes/me/Experience.svelte
+++ src/routes/me/Experience.svelte
@@ -25,7 +25,7 @@
 		</div>
 	</div>
 	<Timeline>
-		<TimelineItem icon={IconPajamas} date="Oct 2018 - now" hidden={!items.Industrial}>
+		<TimelineItem icon={IconPajamas} date="Oct 2018 - June 2023" hidden={!items.Industrial}>
 			<svelte:fragment slot="title">
 				<ExperienceTitle
 					title="Co-Founder and Chief Research Officer (CRO)"
@@ -38,11 +38,11 @@
 				<ExperienceTitle title="Research Assistant" place="The University of Tokyo" />
 			</svelte:fragment>
 		</TimelineItem>
-		<!-- <TimelineItem icon={IconRunning} date="July 2022 - now" hidden={!items.Industrial}> -->
-		<!-- 	<svelte:fragment slot="title"> -->
-		<!-- 		<ExperienceTitle title="Full-Stack Engineer" place="Eightis Inc." link="https://eightis.co.jp/" /> -->
-		<!-- 	</svelte:fragment> -->
-		<!-- </TimelineItem> -->
+		<TimelineItem icon={IconRunning} date="July 2022 - now" hidden={!items.Industrial}>
+			<svelte:fragment slot="title">
+				<ExperienceTitle title="Full-Stack Engineer" place="Eightis Inc." link="https://eightis.co.jp/" />
+			</svelte:fragment>
+		</TimelineItem>
 		<TimelineItem icon={IconTeaching} date="May 2021 - now" hidden={!items.Teaching}>
 			<svelte:fragment slot="title">
 				<ExperienceTitle title="Instructor" place="AI Academy Inc." link="https://aiacademy.jp" />
