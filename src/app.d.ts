97463d82031873e4627f4aa13ee049a2855c1c63
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-11 20:54:17 +0100

    fix how to declare types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,5 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+import 'unplugin-icons/types/svelte';
 
-/// <reference types="unplugin-icons/types/svelte" />
 declare namespace App {
 	// interface Locals {}
 	// interface PageData {}
@@ -10,8 +7,10 @@ declare namespace App {
 	// interface Platform {}
 }
 
-type Post = {
-	title: string;
-	slug: string;
-	pubDate: string;
-};
+declare global {
+	type Post = {
+		title: string;
+		slug: string;
+		pubDate: string;
+	};
+}
