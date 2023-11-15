aee931f502ce3a098db8b906dfe97653658bd75f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-15 10:51:36 +0000

    ensure all props in item are not nullish

--- src/routes/api/rss.json/+server.js
+++ src/routes/api/rss.json/+server.js
@@ -5,18 +5,24 @@ import { is, ensure } from 'unknownutil';
 
 const parser = new Parser();
 
+const isItem = is.ObjectOf({
+	title: is.String,
+	link: is.String,
+	pubDate: is.String
+});
+
 export const GET = async () => {
 	const feeds = (
 		await Promise.all(
 			rss.map(async (url) => {
 				const feed = await parser.parseURL(url);
-				return feed.items.map((item) => ({ title: item.title, link: item.link, pubDate: item.pubDate }));
+				return feed.items.map(({ title, link, pubDate }) => ensure({ title, link, pubDate }, isItem));
 			})
 		)
 	).flat();
 
 	const sortedFeeds = feeds.sort((a, b) => {
-		return new Date(ensure(b.pubDate, is.String)).getTime() - new Date(ensure(a.pubDate, is.String)).getTime();
+		return new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime();
 	});
 
 	return json(sortedFeeds);
