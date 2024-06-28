0bf22de92fc6d466e603ad423b567afec2ac6724
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-28 18:52:21 +0100

    feat: Add automatic favicon generation
    This commit introduces a new feature for automatic favicon generation.
    The 'favicons' npm package is added to the project dependencies. A new
    script 'favicons.js' is created to handle the generation process. The
    favicons are now stored in the 'static/favicons' directory and the
    favicon HTML is stored in 'src/lib/assets/favicons.html'. The favicons
    are now included in the layout via raw import of 'favicons.html'. The
    favicon generation is also integrated into the build process via a new
    Vite plugin defined in 'favicons.js'.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,7 @@
 
 	import Nav from '$lib/Nav.svelte';
 
-	import appleTouchIcon from '$lib/assets/favicons/apple-touch-icon.png';
-	import favicon32 from '$lib/assets/favicons/favicon-32x32.png';
-	import favicon16 from '$lib/assets/favicons/favicon-16x16.png';
-	import siteWebmanifest from '$lib/assets/favicons/site.webmanifest';
+	import faviconLinks from '$lib/assets/favicons.html?raw';
 	import ryoppippi from '$lib/assets/ryoppippi.png';
 
 	onNavigate((navigation) => {
@@ -30,30 +27,12 @@
 	const title = `ryoppippi.com`;
 </script>
 
+<svelte:head>
+	<!-- eslint-disable svelte/no-at-html-tags -->
+	{@html faviconLinks}
+</svelte:head>
+
 <MetaTags
-	additionalLinkTags={[
-		{
-			rel: 'apple-touch-icon',
-			href: appleTouchIcon,
-			sizes: '180x180',
-		},
-		{
-			rel: 'icon',
-			type: 'image/png',
-			href: favicon32,
-			sizes: '32x32',
-		},
-		{
-			rel: 'icon',
-			type: 'image/png',
-			href: favicon16,
-			sizes: '16x16',
-		},
-		{
-			rel: 'manifest',
-			href: siteWebmanifest,
-		},
-	]}
 	additionalRobotsProps={{
 		noarchive: true,
 		nosnippet: true,
