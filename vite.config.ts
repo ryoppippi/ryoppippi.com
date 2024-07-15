92a42dcd2e42ce746476f9b65c4cbbc518b17a7c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-15 12:10:40 +0100

    feat: add cloudflare redirect plugin to vite config
    This commit introduces the '@ryoppippi/vite-plugin-cloudflare-redirect'
    plugin to the Vite configuration. The plugin is set to generate mode
    and is configured to redirect from '/cv' to 'https://cv.ryoppippi.com'
    with a status of 301. The plugin and its dependencies are also added to
    the package.json and the lock file is updated accordingly.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,17 @@ import Macros from '@unplugin/macros/vite';
 import Icons from 'unplugin-icons/vite';
 import UnpluginTypia from '@ryoppippi/unplugin-typia/vite';
 import { isCI } from 'std-env';
+import { cloudflareRedirect } from '@ryoppippi/vite-plugin-cloudflare-redirect';
 import { faviconPlugin } from './plugins/favicons';
 
 export default defineConfig({
 	plugins: [
+		cloudflareRedirect({
+			mode: 'generate',
+			entries: [
+				{ from: '/cv', to: 'https://cv.ryoppippi.com', status: 301 },
+			],
+		}),
 		faviconPlugin,
 		UnpluginTypia({ cache: !isCI, log: 'verbose' }),
 		enhancedImages(),
