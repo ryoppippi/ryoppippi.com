5cd6583230003da9ad7adbf8555bb533318e74f1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 20:38:42 +0100

    feat(Social.svelte): replace icon components with classes
    This commit replaces the icon components in Social.svelte with
    corresponding class names. This change simplifies the code and
    improves maintainability. The icon components were replaced with
    their corresponding class names in the ICONS array.

--- src/lib/Social.svelte
+++ src/lib/Social.svelte
@@ -2,33 +2,26 @@
 	import { fade } from 'svelte/transition';
 	import { parseURL } from 'ufo';
 
-	import IconGithub from '~icons/line-md/github-loop';
-	import IconZenn from '~icons/simple-icons/zenn';
-	import IconLinkedin from '~icons/line-md/linkedin';
-	import IconTwitter from '~icons/line-md/twitter';
-	import IconReddit from '~icons/line-md/reddit-loop';
-	import IconYoutube from '~icons/ri/youtube-line';
-	import IconBluesky from '~icons/simple-icons/bluesky';
-
 	const { size = 4.5 } = $props();
 
 	const ICONS = [
-		{ component: IconGithub, url: 'https://github.com/ryoppippi' },
-		{ component: IconZenn, url: 'https://zenn.dev/ryoppippi' },
-		{ component: IconLinkedin, url: 'https://www.linkedin.com/in/ryoppippi/' },
-		{ component: IconTwitter, url: 'https://twitter.com/ryoppippi' },
-		{ component: IconBluesky, url: 'https://bsky.app/profile/ryoppippi.com' },
-		{ component: IconReddit, url: 'https://www.reddit.com/user/ryoppippi' },
-		{ component: IconYoutube, url: 'https://www.youtube.com/channel/UCJbUM-yZx6mESJw82-OpMuQ' },
+		{ class: 'i-line-md:github-loop', url: 'https://github.com/ryoppippi' },
+		{ class: 'i-simple-icons:zenn', url: 'https://zenn.dev/ryoppippi' },
+		{ class: 'i-line-md:linkedin', url: 'https://www.linkedin.com/in/ryoppippi/' },
+		{ class: 'i-line-md:linkedin', url: 'https://twitter.com/ryoppippi' },
+		{ class: 'i-simple-icons:bluesky', url: 'https://bsky.app/profile/ryoppippi.com' },
+		{ class: 'i-line-md:reddit-loop', url: 'https://www.reddit.com/user/ryoppippi' },
+		{ class: 'i-ri:youtube-line', url: 'https://www.youtube.com/channel/UCJbUM-yZx6mESJw82-OpMuQ' },
 	] as const;
 </script>
 
 <article class='grid grid-cols-3 place-items-center gap-3 sm:grid-cols-7' in:fade|global={{ duration: 3000 }}>
-	{#each ICONS as { component, url } (url)}
+	{#each ICONS as { class: _class, url } (url)}
 		{@const { host } = parseURL(url)}
 		<div class='animation cursor-pointer'>
 			<a aria-label="link to ryoppippi's {host}" href={url} rel='noopener noreferrer' target='_blank'>
-				<svelte:component this={component} style='font-size: {size}vh' />
+				<!-- svelte-ignore element_invalid_self_closing_tag -->
+				<div style:font-size='{size}vh' class={_class} />
 			</a>
 		</div>
 	{/each}
