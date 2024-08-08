f2feb42e1055d04d24900c334390ad6ada4828a4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 22:59:15 +0100

    feat: update presetAttributify configuration
    The configuration for presetAttributify in uno.config.ts has been
    updated. The prefix and prefixedOnly options have been removed to
    simplify the configuration and improve compatibility with other
    components.

--- uno.config.ts
+++ uno.config.ts
@@ -11,7 +11,7 @@ import {
 export default defineConfig({
 	presets: [
 		presetUno(),
-		presetAttributify({ prefix: 'uno-', prefixedOnly: true }),
+		presetAttributify(),
 		presetIcons({ autoInstall: isDevelopment }),
 	],
 	transformers: [
