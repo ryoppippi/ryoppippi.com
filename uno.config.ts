e41a93925eff8d4948a69de18054803cc160cf21
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 21:01:04 +0100

    feat: remove unplugin-icons and related dependencies
    This commit removes the unplugin-icons package and related iconify
    dependencies from the project. It also removes the import statement
    for unplugin-icons in the 'src/app.d.ts' file. Additionally, it adds
    new color definitions in the uno.config.ts file.

--- uno.config.ts
+++ uno.config.ts
@@ -20,6 +20,16 @@ export default defineConfig({
 	],
 	theme: {
 		colors: {
+			'primary-100': '#FF6B6B',
+			'primary-200': '#dd4d51',
+			'primary-300': '#8f001a',
+			'accent-100': '#FFE66D',
+			'accent-200': '#958500',
+			'text-100': '#FFFFFF',
+			'text-200': '#e0e0e0',
+			'bg-100': '#1E1E1E',
+			'bg-200': '#2d2d2d',
+			'bg-300': '#454545',
 		},
 		breakpoints: {
 			tiny: '375px',
