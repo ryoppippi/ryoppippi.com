fbf7f1ca54e810f24c47ef21c9ac88e85738f130
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 20:38:53 +0100

    feat(TimelineItem): replace Svelte components with string icons
    The `icon` prop in `TimelineItem.svelte` has been changed from a Svelte
    component to a string. This change simplifies the usage of `TimelineItem`
    by allowing the use of icon classes instead of importing and passing
    Svelte components. The `Experience.svelte` file has been updated to
    reflect this change.

--- src/lib/TimelineItem.svelte
+++ src/lib/TimelineItem.svelte
@@ -10,7 +10,7 @@
 		mainSlot,
 		otherSlot,
 	}: {
-		icon: ConstructorOfATypedSvelteComponent;
+		icon: string;
 		title?: string;
 		date: string;
 		hidden?: boolean;
@@ -21,8 +21,9 @@
 </script>
 
 <li class='mb-10 ml-6 transition-opacity' class:opacity-20={hidden}>
+	<!-- svelte-ignore element_invalid_self_closing_tag -->
 	<span class='flex h-6 w-6 items-center justify-center rounded-full bg-me-primary-200 ring-8 ring-gray-900 md:absolute md:-left-3'>
-		<svelte:component this={icon} />
+		<div class={icon} />
 	</span>
 
 	<h3 class='mb-1 flex items-center text-lg font-semibold text-me-text-100'>
