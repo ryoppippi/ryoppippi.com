2ad51786a5d2e28455084510cdd07e8d7477d97d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-08 21:01:04 +0100

    feat: remove unplugin-icons and related dependencies
    This commit removes the unplugin-icons package and related iconify
    dependencies from the project. It also removes the import statement
    for unplugin-icons in the 'src/app.d.ts' file. Additionally, it adds
    new color definitions in the uno.config.ts file.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import Macros from '@unplugin/macros/vite';
-import Icons from 'unplugin-icons/vite';
 import UnpluginTypia from '@ryoppippi/unplugin-typia/vite';
-import { isCI } from 'std-env';
 import { cloudflareRedirect } from '@ryoppippi/vite-plugin-cloudflare-redirect';
 
 import extractorSvelte from '@unocss/extractor-svelte';
@@ -44,10 +42,6 @@ export default defineConfig({
 			extractors: [
 				extractorSvelte(),
 			],
-		}),
-		Icons({
-			compiler: 'svelte',
-			autoInstall: true,
 		}),
 		sveltekit(),
 	],
