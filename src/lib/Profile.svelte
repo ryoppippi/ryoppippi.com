119f597ef3804d38c2301430de5979b99f6ee019
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-14 17:23:41 +0100

    fix(Profile.svelte): class

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -7,7 +7,7 @@
 <div>
 	<enhanced:img
 		style:--tag={name}
-		class='object-fit mx-auto aspect-square w-1/2 rounded-full md:h-64 md:w-64'
+		class='object-fit mx-auto aspect-square w-1/2 rounded-full md:(h-64 w-64)'
 		alt='profile'
 		src={img} />
 </div>
