024327f1e082a17c0b102f4d34c5cadfa80a64a4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-03 11:19:27 +0100

    feat(Nav.svelte): fix style

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -7,64 +7,62 @@
 		// { link: '/works', name: 'works' },
 		{ href: '/blog', name: 'blog' },
 	] as const satisfies Readonly<{ href: string; name: string }[]>;
-	const CVMessage = `My CV `;
 </script>
 
-<header py-6>
-	<div
-		container
-		flex='~ col wrap md:row'
+<header
+	container
+	flex='~ col wrap md:row'
+	items-center
+	max-w-8xl
+	mxa
+	p-5
+	py-6
+	text-text-100
+>
+	<a
+		flex
+		font-bold
+		href='/'
 		items-center
-		max-w-8xl
-		mx-auto
-		p-5
-		text-text-100
+		mb='md:0'
 	>
-		<a
-			flex
-			font-bold
-			href='/'
-			items-center
-			mb='md:0'
-		>
-			{TITLE}
-		</a>
-		<nav
-			flex='~ wrap'
-			gap-3
-			items-center
-			justify-center
-			mx='md:auto'
-			text-base
-		>
-			{#each LINKS as { href, name } (href)}
-				<a
-					block
-					border='b-2 transparent hover:primary-100'
-					font-bold
-					{href}
-					px-0
-					py-3
-					text-xl
-				>
-					{name}
-				</a>
-			{/each}
-		</nav>
-		<!-- svelte-ignore element_invalid_self_closing_tag -->
-		<a
-			btn
-			font-bold
-			href='https://cv.ryoppippi.com'
-			mt='4 md:0'
-			opacity='90 hover:100'
-			px-6
-			py-2
-			target='_blank'
-			variant='glass-primary hover:filled-primary'
-		>
-			{CVMessage}
-			<div class='i-line-md:download-outline' />
-		</a>
-	</div>
+		{TITLE}
+	</a>
+	<nav
+		flex='~ wrap'
+		gap-3
+		items-center
+		justify-center
+		mxa
+		text-base
+	>
+		{#each LINKS as { href, name } (href)}
+			<a
+				block
+				border='b-2 transparent hover:primary-100'
+				font-bold
+				{href}
+				px-0
+				py-3
+				text-xl
+			>
+				{name}
+			</a>
+		{/each}
+	</nav>
+	<!-- svelte-ignore element_invalid_self_closing_tag -->
+	<a
+		flex='~'
+		font-bold
+		href='https://cv.ryoppippi.com'
+		items-center
+		mt='4 md:0'
+		opacity='90 hover:100'
+		px-6
+		py-2
+		target='_blank'
+		variant='glass-primary hover:filled-primary'
+	>
+		My CV <span class='i-line-md:download-outline' />
+	</a>
 </header>
