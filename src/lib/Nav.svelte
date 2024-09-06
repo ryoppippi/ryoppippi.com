1af98db4e388bf132f0a8c765080186b89b5bc71
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-06 21:11:52 +0100

    feat(Nav.svelte): cv as one of the menu

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -7,9 +7,10 @@
 
 	const TITLE = 'ryoppippi' as const satisfies string;
 	const LINKS = [
-		{ href: '/works', name: 'works' },
-		{ href: '/blog', name: 'blog' },
-	] as const satisfies Readonly<{ href: string; name: string }[]>;
+		{ name: 'works', href: '/works' },
+		{ name: 'blog', href: '/blog' },
+		{ name: 'cv', href: joinURL(process.env.DOMAIN as string, '/cv'), icon: 'i-line-md:download-outline' },
+	] as const satisfies { href: string; name: string; icon?: string }[];
 </script>
 
 <header
@@ -41,30 +42,26 @@
 		m='xa md:r0'
 		text-lg
 	>
-		{#each LINKS as { href, name } (href)}
+		{#each LINKS as { href, name, ...rest } (href)}
 			{@const isPath = $page.url.pathname === href}
+			{@const icon = 'icon' in rest ? rest.icon : null}
 			<a
 				class:border-primary-100={isPath}
 				class:border-transparent={!isPath}
 				block
 				border='b-2 hover:primary-100'
+				flex='~'
 				{href}
+				items-center
 				px-0
 				py-3
+				tatget={href.startsWith('http') ? '_blank' : undefined}
 			>
 				{name}
+				<!-- svelte-ignore element_invalid_self_closing_tag -->
+				{#if icon != null} <span class={icon} /> {/if}
 			</a>
 		{/each}
-		<a
-			flex='~'
-			href={joinURL(process.env.DOMAIN as string, '/cv')}
-			items-center
-			opacity='90 hover:100'
-			target='_blank'
-		>
-			<!-- svelte-ignore element_invalid_self_closing_tag -->
-			CV <span class='i-line-md:download-outline' />
-		</a>
 		<DarkMode.ToggleButton />
 	</nav>
 </header>
