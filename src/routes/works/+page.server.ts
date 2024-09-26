bf857ba9e9e83caa04501b40eb7ca4ef0d85a56f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-26 11:17:56 +0100

    fix: delete cache feature

--- src/routes/works/+page.server.ts
+++ src/routes/works/+page.server.ts
@@ -2,27 +2,15 @@ import type { Entries } from 'type-fest';
 import { joinURL } from 'ufo';
 import type { tags } from 'typia';
 import typia from 'typia';
-import { isEqual } from 'ohash';
 import type { PageServerLoad } from './$types';
 import type { GHRepo, Project, Projects } from './projects';
 import _projects from './projects';
-import { Json, getCachePath } from '$lib/cache';
 
 const GITHUB_URL = `https://github.com`;
 
 type ReturnType = Readonly<Record<keyof typeof _projects, (Required<Project> & GHRepo['repo'])[]>>;
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const cachePath = getCachePath('projects', _projects);
-	using cacheJson = new Json<ReturnType>(cachePath, { allowNoExist: true });
-
-	/* if null or empty */
-	if (!typia.is<EmptyObject | null | undefined>(cacheJson.data)) {
-		return {
-			projects: cacheJson.data,
-		};
-	}
-
 	const writableProjects: Projects = _projects;
 	const fetchPromises: Promise<void>[] = [];
 
@@ -68,10 +56,6 @@ export const load: PageServerLoad = async ({ fetch }) => {
 	await Promise.all(fetchPromises);
 
 	const _writableProjects = writableProjects as ReturnType;
-
-	if (!isEqual(cacheJson.data, _writableProjects)) {
-		cacheJson.data = _writableProjects;
-	}
 
 	return {
 		projects: _writableProjects,
