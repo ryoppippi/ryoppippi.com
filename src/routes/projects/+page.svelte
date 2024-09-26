380784c4cb2ea7f018d5edbedcde2ea1a8ff5e90
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-26 18:13:14 +0100

    feat(projects): fix texts

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -58,23 +58,30 @@
 		</div>
 	</div>
 
-	{#each Object.entries(projects) as [genre, projectsByGenrne], count (genre)}
-		<div style:--stagger={count}>
-			<h2
-				op50
-				py-8
-				text='4xl stroke-hex-aaa'
-			>
-				{genre}
-			</h2>
-			<div
-				class='[--delay:300ms] sm:[--delay:450ms]'
-				grid='~ cols-1 md:cols-2 gap-8'
-			>
-				{#each projectsByGenrne as project (project.link)}
-					<ProjectCard {project} />
-				{/each}
+	<div grid='~ gap-16'>
+		{#each Object.entries(projects) as [genre, projectsByGenrne], count (genre)}
+			<div style:--stagger={count}>
+				<h2
+					font='mono bold'
+					line-height-none
+					op='35 dark:20'
+					py-8
+					select-none
+					text='size-4.5em transparent'
+					text-stroke='hex-aaa [1.5px]'
+				>
+					{genre}
+				</h2>
+				<div
+					class='[--delay:300ms] sm:[--delay:450ms]'
+					grid='~ cols-1 md:cols-2 gap-8'
+				>
+					{#each projectsByGenrne as project (project.link)}
+						<ProjectCard {project} />
+					{/each}
+				</div>
+
 			</div>
-		</div>
-	{/each}
+		{/each}
+	</div>
 </div>
