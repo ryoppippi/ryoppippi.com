b948897c4f8ae9dd4eea658433689b2888ce252d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-26 11:16:32 +0100

    featfix(works/+page.server.ts): make it parallel

--- src/routes/works/+page.server.ts
+++ src/routes/works/+page.server.ts
@@ -1,5 +1,6 @@
 import type { Entries } from 'type-fest';
 import { joinURL } from 'ufo';
+import type { tags } from 'typia';
 import typia from 'typia';
 import { isEqual } from 'ohash';
 import type { PageServerLoad } from './$types';
@@ -9,7 +10,6 @@ import { Json, getCachePath } from '$lib/cache';
 
 const GITHUB_URL = `https://github.com`;
 
-type EmptyObject = { [key: string]: never };
 type ReturnType = Readonly<Record<keyof typeof _projects, (Required<Project> & GHRepo['repo'])[]>>;
 
 export const load: PageServerLoad = async ({ fetch }) => {
@@ -24,39 +24,48 @@ export const load: PageServerLoad = async ({ fetch }) => {
 	}
 
 	const writableProjects: Projects = _projects;
+	const fetchPromises: Promise<void>[] = [];
+
 	for (const [gerne, projects] of Object.entries(writableProjects) as Entries<typeof _projects>) {
 		for (const [index, project] of projects.entries()) {
 			let originalProject = structuredClone(project as Project);
-			if (originalProject?.link == null) {
+			if (!typia.is<string>(originalProject?.link)) {
 				originalProject = {
 					...originalProject,
 					link: joinURL(GITHUB_URL, 'ryoppippi', project.name),
 				};
 			}
-			if (
-				originalProject.description == null
-				&& originalProject?.link?.startsWith(GITHUB_URL) === true
-			) {
-				try {
-					const unghURL = originalProject.link.replace(GITHUB_URL, 'https://ungh.cc/repos');
-					const ghRepo = await fetch(unghURL);
-					if (!ghRepo.ok) {
-						throw new Error(`Failed to fetch ${unghURL}`);
+			if (!typia.is<string>(originalProject?.description)) {
+				const fetchPromise = (async () => {
+					try {
+						const unghURL = originalProject.link?.replace(GITHUB_URL, 'https://ungh.cc/repos').trim() ?? '';
+						typia.assertGuard<string & tags.Format<'url'>>(unghURL);
+
+						const ghRepoRes = await fetch(unghURL);
+						if (!ghRepoRes.ok) {
+							throw new Error(`Failed to fetch ${unghURL}`);
+						}
+						const ghRepo = await ghRepoRes.json();
+						typia.assertGuard<GHRepo>(ghRepo);
+						originalProject = {
+							...originalProject,
+							...ghRepo.repo,
+						};
 					}
-					const t = await ghRepo.json();
-					const r = typia.assert<GHRepo>(t);
-					originalProject = {
-						...originalProject,
-						...r.repo,
-					};
-				}
-				catch (e) {
-					console.error(e);
-				}
+					catch (e) {
+						console.error(e);
+					}
+					writableProjects[gerne][index] = originalProject;
+				})();
+				fetchPromises.push(fetchPromise);
 			}
-			writableProjects[gerne][index] = originalProject;
-		};
+			else {
+				writableProjects[gerne][index] = originalProject;
+			}
+		}
 	}
+
+	await Promise.all(fetchPromises);
 
 	const _writableProjects = writableProjects as ReturnType;
 
