2966d4dd3c7f2ba9643c68a62159f2ea117634a9
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-27 22:09:50 +0100

    feat(blog): move to contents

--- src/contents/blog/index.ts
+++ src/contents/blog/index.ts
@@ -1,7 +1,8 @@
 import sortOn from 'sort-on';
+import typia from 'typia';
+import type { Item, Metadata } from '$contents/blog/types';
 
 import { parseMarkdown } from '$lib/markdown.server';
-import type { Metadata } from '$contents/blog/types';
 
 async function getPosts() {
 	const originals = import.meta.glob('$contents/blog/*.md', { eager: true, as: 'raw' });
@@ -15,7 +16,10 @@ async function getPosts() {
 			const metadata = await parseMarkdown<Metadata>(mdRaw);
 
 			if (metadata.isPublished) {
-				return { ...metadata, slug };
+				return typia.assert<Item>({
+					...metadata,
+					slug,
+				});
 			}
 		}
 		catch (e) {
