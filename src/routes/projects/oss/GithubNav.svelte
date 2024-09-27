e50e87e72dea420046dfa41b8713188ec76d1dc3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-27 20:03:29 +0100

    feat(projects/GithubNav.svelte): use snippets

--- src/routes/projects/oss/GithubNav.svelte
+++ src/routes/projects/oss/GithubNav.svelte
@@ -1,6 +1,24 @@
-<script>
+<script lang='ts'>
 	import { subdomain } from '$lib/util';
 </script>
+
+{#snippet link(
+	_subdomain: string,
+	icon: string,
+	color: `btn-${string}`,
+	name: string
+)}
+		<a
+			class={color}
+			fcol-md-row
+			fyc
+			href={subdomain(_subdomain)}
+			target='_blank'
+		>
+			<!-- svelte-ignore element_invalid_self_closing_tag -->
+			<span class={icon} /> {name}
+		</a>
+{/snippet}
 
 <div
 	mt10
@@ -9,36 +27,9 @@
 	prose='~ dark:invert'
 >
 	<div fxc gap-2>
-		<a
-			btn-green
-			fcol-md-r
-			fyc
-			href={subdomain('/pr')}
-			target='_blank'
-		>
-			<!-- svelte-ignore element_invalid_self_closing_tag -->
-			<span i-ph-git-pull-request-duotone /> My Recent PRs
-		</a>
-		<a
-			btn-blue
-			fcol-md-r
-			fyc
-			href={subdomain('/gh')}
-			target='_blank'
-		>
-			<!-- svelte-ignore element_invalid_self_closing_tag -->
-			<span i-ph-github-logo-duotone /> GitHub
-		</a>
-		<a
-			btn-pink
-			fcol-md-r
-			fyc
-			href={subdomain('/gh-by-stars')}
-			target='_blank'
-		>
-			<!-- svelte-ignore element_invalid_self_closing_tag -->
-			<span i-ph-star /> Sort by Stars
-		</a>
+		{@render link('/pr', 'i-ph-git-pull-request-duotone', 'btn-green', 'My Recent PRs')}
+		{@render link('/gh', 'i-ph-github-logo-duotone', 'btn-blue', 'GitHub')}
+		{@render link('/gh-by-stars', 'i-ph-star', 'btn-pink', 'Sort by Stars')}
 	</div>
 
 </div>
