effc915881e45c31ad58c29542ec1aae922c892b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-27 15:32:50 +0100

    feat(project): use fluid text

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -74,13 +74,14 @@
 				data-sliding-animate
 			>
 				<h2
+					f-text-32-64
 					font='mono bold'
 					line-height-none
 					op='35 dark:20'
 					py-8
 					select-none
-					text='size-4.0em transparent'
 					text-stroke='hex-aaa [1.5px]'
+					text-transparent
 				>
 					{genre}
 				</h2>
