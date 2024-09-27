66f6affff5ad341dd20e70f7e077378260781856
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-27 19:02:53 +0100

    feat(projects): sectionを追加

--- src/routes/projects/+layout.svelte
+++ src/routes/projects/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang='ts'>
+	import { capitalize } from '@ryoppippi/str-fns';
 	import ProjectTitle from './ProjectTitle.svelte';
 	import Title from '$lib/Title.svelte';
+	import { page } from '$app/stores';
+
+	const routes = [
+		'oss',
+		'art',
+	] as const;
 
 	const { children } = $props();
 </script>
@@ -13,10 +20,19 @@
 	</div>
 
 	<div
-		flex='~ col gap-1 sm:row sm:gap-3 wrap'
+		flex='~ col sm:row wrap'
+		gap='1 sm:3'
 		mb-0
-		text-3xl>
-		<a href='/projects'>OSS</a>
+		text-3xl
+	>
+		{#each routes as route (route)}
+			{@const isCurrent = $page.route.id?.endsWith(route)}
+			<a
+				class:op100={isCurrent}
+				href={isCurrent ? null : route}
+				op50
+			>{capitalize(route)}</a>
+		{/each}
 	</div>
 
 	{@render children()}
