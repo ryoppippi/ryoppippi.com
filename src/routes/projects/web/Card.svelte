a70d691121ba70675767c39d3277caf5b2cf1cbd
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-28 11:08:06 +0100

    feat(projects/web): card.svelte

--- src/routes/projects/web/Card.svelte
+++ src/routes/projects/web/Card.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 	import type { Project } from '$contents/projects/web';
+	import { formatDate } from '$lib/util';
 
 	type Props = {
 		project: Project;
@@ -11,27 +12,45 @@
 </script>
 
 <div
+	class='group'
 	border='~ base rounded-lg'
 	hover='scale-101 shadow-xl z-10'
+	select-none
 	target={project.link.startsWith('http') ? '_blank' : '_self'}
-	transition='all duration-500'
+	transition-base
 >
 	<a href={project.link}>
 		{#if typeof project.image === 'string'}
 			<img
 				alt={project.title}
+				border='b base'
 				src={project.image}
 			/>
 		{:else}
 			<enhanced:img
 				alt={project.title}
+				border='b base'
 				src={project.image}
 			/>
 		{/if}
 
-		<div border-none m0 ma p4 pb3 prose-base>
+		<div
+			m0
+			mt--8
+			op-card
+			p4
+			pb3
+			prose='base sm'
+			transition-base
+		>
+			<h3> {project.title}</h3>
 			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 			{@html project.content}
+			{#if project.pubDate}
+				<div op50 pt2 text-sm>
+					{formatDate(new Date(project.pubDate))}
+				</div>
+			{/if}
 		</div>
 	</a>
 </div>
