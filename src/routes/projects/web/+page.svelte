57392762fb66c81ef1e9fd6e089a5e2b762d9564
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-28 10:24:31 +0100

    feat(projects/web): add web page

---
+++ src/routes/projects/web/+page.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+	import Card from './Card.svelte';
+
+	const { data } = $props();
+	const { projects } = data;
+</script>
+
+<div
+	grid='~ gap-4'
+	grid-cols='1 lg:2'
+>
+	{#each projects as project (project.link)}
+		<Card {project} />
+	{/each}
+</div>
---
+++ src/routes/projects/web/Card.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+	import type { Project } from '$contents/projects/web';
+
+	type Props = {
+		project: Project;
+	};
+
+	const {
+		project,
+	}: Props = $props();
+</script>
+
+<div
+	border='~ base rounded-lg'
+	hover='scale-101 shadow-xl z-10'
+	target={project.link.startsWith('http') ? '_blank' : '_self'}
+	transition='all duration-500'
+>
+	<a href={project.link}>
+		{#if typeof project.image === 'string'}
+			<img
+				alt={project.title}
+				src={project.image}
+			/>
+		{:else}
+			<enhanced:img
+				alt={project.title}
+				src={project.image}
+			/>
+		{/if}
+
+		<div border-none m0 ma p4 pb3 prose-base>
+			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+			{@html project.content}
+		</div>
+	</a>
+</div>
