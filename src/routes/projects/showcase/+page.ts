bb7c08a4ea4c3748b695351d0fda60f0249ce303
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-29 00:37:34 +0100

    fix(showcase): parse image files

--- src/routes/projects/showcase/+page.ts
+++ src/routes/projects/showcase/+page.ts
@@ -1,3 +1,4 @@
+import { parseFilename } from 'ufo';
 import type { PageLoad } from './$types';
 
 type EnhancedImg = typeof import('*.png?enhanced').default;
@@ -5,7 +6,10 @@ type EnhancedImg = typeof import('*.png?enhanced').default;
 export const load = (({ data }) => {
 	const images: Record<string, { default: EnhancedImg }> = import.meta.glob('$contents/projects/showcase/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp,svg}', { eager: true, query: { enhanced: true } });
 	const projects = data.projects.map((project) => {
-		const [_, enhancedImg] = Object.entries(images).find(([key]) => project.image != null && project.image.endsWith(key)) ?? [];
+		const [_, enhancedImg] = Object.entries(images).find(([key]) => (parseFilename(key, { strict: true }) === parseFilename(project.image, { strict: true }))) ?? [];
+		if (enhancedImg == null) {
+			console.error(`Image not found for project: ${project.title}`);
+		}
 		return {
 			...project,
 			image: enhancedImg,
