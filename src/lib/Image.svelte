cfc3ab7e869f13a2ddf88b57a30f40cb0d702b20
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-29 21:55:17 +0100

    feat(Image.svelte): load eagerly

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -8,18 +8,19 @@
 
 	const { src, alt, ...rest }: Props = $props();
 
-	async function importImage(src: string): Promise<EnhancedImg> {
+	function importImage(src: string): EnhancedImg {
 		const pictures = import.meta.glob(`/src/**/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}`, {
 			import: 'default',
 			query: {
 				enhanced: true,
 				w: '2400;2000;1600;1200;800;400',
 			},
+			eager: true,
 		});
 
 		for (const [path, picutureSrc] of Object.entries(pictures)) {
 			if (path.includes(src) || src.includes(path)) {
-				return await picutureSrc() as Promise<EnhancedImg>;
+				return picutureSrc as EnhancedImg;
 			}
 		}
 		throw new Error(`Image not found: ${src}`);
@@ -34,12 +35,10 @@
 		{src}
 	/>
 {:else}
-	{#await importImage(src) then src}
-		<enhanced:img
-			{alt}
-			{...rest}
-			loading='lazy'
-			{src}
-		/>
-	{/await}
+	<enhanced:img
+		{alt}
+		{...rest}
+		loading='lazy'
+		src={importImage(src)}
+	/>
 {/if}
