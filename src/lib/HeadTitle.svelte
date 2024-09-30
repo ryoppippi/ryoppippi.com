94eb99356eaa367694d20ae6a1c275ba0fe773ce
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-30 11:05:05 +0100

    fix(HeadTitle.svelte): fix title

--- src/lib/HeadTitle.svelte
+++ src/lib/HeadTitle.svelte
@@ -3,10 +3,11 @@
 
 	const { title }: { title?: string } = $props();
 
-	const displayTitle = (title != null ? `${title} |` : '') + domain();
+	const displayTitle = (title != null ? `${title} | ` : '') + domain();
 </script>
 
 <svelte:head>
 	<title> {displayTitle} </title>
 	<meta content={displayTitle} property='og:title' />
+	<meta name='twitter:title' content={displayTitle} />
 </svelte:head>
