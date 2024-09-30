292a8297c22a7b55f71a5bd012ffa0889d8a46cd
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-30 18:35:10 +0100

    chore: remove sveltekit-preprocess-import-assets

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,15 @@ import path from 'node:path';
 import process from 'node:process';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { importAssets } from 'svelte-preprocess-import-assets';
 import { isDevelopment } from 'std-env';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), importAssets()],
+	preprocess: [
+		vitePreprocess(),
+	],
 
 	vitePlugin: {
 		inspector: isDevelopment,
