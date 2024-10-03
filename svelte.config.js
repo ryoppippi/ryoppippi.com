ef6d0ddc8936723e0c47d2da5c523e9eb55d77d7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-03 10:17:17 +0100

    feat(markdown): move markdown-it config to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,69 @@ import adapter from '@sveltejs/adapter-static';
 import { isDevelopment } from 'std-env';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { importAssets } from 'svelte-preprocess-import-assets';
+
+import markdownit from 'markdown-it';
+
+import anchor from 'markdown-it-anchor';
+
+import MarkdownItShiki from '@shikijs/markdown-it';
+import { rendererRich, transformerTwoslash } from '@shikijs/twoslash';
+
+import GitHubAlerts from 'markdown-it-github-alerts';
+
+// @ts-expect-error no types
+import Figures from 'markdown-it-image-figures';
+
+import LinkAttributes from 'markdown-it-link-attributes';
+
+import { slugify } from './src/lib/util.js';
 import svelteMarkdown from './src/markdown/preprocessor.js';
-import { md } from './src/markdown/markdown-it.js';
+import { transformerEscape } from './src/markdown/shiki-transformer.js';
+
+const md = markdownit({
+	html: true,
+	linkify: true,
+	typographer: true,
+});
+
+md.use(anchor, {
+	slugify,
+	permalink: anchor.permalink.linkInsideHeader({
+		symbol: '#',
+		renderAttrs: () => ({ 'aria-hidden': 'true' }),
+	}),
+});
+
+md.use(LinkAttributes, {
+	/** @param {string} link */
+	matcher: link => /^https?:\/\//.test(link),
+	attrs: {
+		target: '_blank',
+		rel: 'noopener',
+	},
+});
+
+md.use(await MarkdownItShiki({
+	themes: {
+		dark: 'vitesse-dark',
+		light: 'vitesse-light',
+	},
+	transformers: [
+		transformerTwoslash({
+			explicitTrigger: true,
+			renderer: rendererRich(),
+		}),
+		transformerEscape(),
+	],
+}));
+
+md.use(GitHubAlerts);
+
+md.use(Figures, {
+	figcaption: true,
+	lazy: true,
+	async: true,
+});
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
