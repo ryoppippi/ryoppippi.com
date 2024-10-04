e9f0e0508b350f98bd55c15c6a77e62e7cf94252
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-04 17:28:16 +0100

    feat(contents/blog): rewrite with pipe

--- src/contents/blog/index.ts
+++ src/contents/blog/index.ts
@@ -1,30 +1,37 @@
+import { pipe } from '@core/pipe';
+import { filter, flatten, map, reduce } from '@core/iterutil/pipe';
 import typia from 'typia';
 import sortOn from 'sort-on';
 import { dev } from '$app/environment';
 import type { Item, Metadata } from '$contents/blog/types';
 
 export const blogPosts = sortOn(
-	Object.entries(import.meta.glob('$contents/blog/*.md', { eager: true }))
-		.map(([filepath, md]) => {
-			const slug = filepath.split('/').at(-1)?.replace('.md', '');
-			// eslint-disable-next-line ts/no-unsafe-member-access
-			const metadata = typia.assert<Metadata>((md as any).metadata);
-			if (slug == null) {
-				return undefined;
-			}
+	Array.from(
+		pipe(
+			Object.entries(import.meta.glob('$contents/blog/*.md', { eager: true })),
 
-			try {
-				if (dev || metadata.isPublished) {
-					return typia.assert<Item>({
-						...metadata,
-						slug,
-					});
-				}
-			}
-			catch (e) {
-				console.error(e);
-			}
-			return undefined;
-		}).filter(p => p != null),
+			map(([filepath, md]) => ({
+				// eslint-disable-next-line ts/no-unsafe-member-access
+				...typia.assert<Metadata>((md as any).metadata),
+				filepath,
+			})),
+
+			map(({ filepath, ...metadata }) => {
+				const slug = filepath.split('/').at(-1)?.replace('.md', '');
+				return {
+					...metadata,
+					slug,
+				};
+			}),
+			/** filter out files without slug */
+			filter(({ slug }) => slug != null),
+
+			/** correct type */
+			map(({ slug, ...v }) => ({ ...v, slug: slug as string })),
+
+			/** filter isPublished */
+			filter(({ isPublished }) => dev || isPublished),
+		),
+	),
 	['-pubDate'],
-);
+) satisfies Item[];
