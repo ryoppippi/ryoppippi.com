82b5f53df17a950315fa6db33d12e5303b8f7211
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-09 13:57:24 +0100

    feat: unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,18 @@ import svelte from '@astrojs/svelte';
 
 import mdx from '@astrojs/mdx';
 
+import extractorSvelte from '@unocss/extractor-svelte';
+import UnoCSS from 'unocss/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), mdx()],
+	integrations: [
+		svelte(),
+		mdx(),
+		UnoCSS({
+			extractors: [
+				extractorSvelte(),
+			],
+		}),
+	],
 });
