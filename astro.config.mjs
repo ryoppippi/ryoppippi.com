5451a8d0406601dccf65f2de69b1a88fbc5126b1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-09 13:59:25 +0100

    feat: add astro meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,15 @@ import mdx from '@astrojs/mdx';
 import extractorSvelte from '@unocss/extractor-svelte';
 import UnoCSS from 'unocss/astro';
 
+import metaTags from 'astro-meta-tags';
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		svelte(),
 		mdx(),
-		UnoCSS({
-			extractors: [
-				extractorSvelte(),
-			],
-		}),
+		UnoCSS({ extractors: [extractorSvelte()] }),
+		metaTags(),
 	],
 });
+
