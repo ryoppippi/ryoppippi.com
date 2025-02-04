5fec6b89cbfaf1f21e1a564301d0a6965f8c18a1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-04 17:59:17 +0000

    feat(blog): move blog contents

--- blog/src/lib/index.ts
+++ blog/src/lib/index.ts
@@ -1,1 +1,30 @@
-// Reexport your entry components here
+import type { Item, Metadata } from './types.js';
+import { dev } from '$app/environment';
+import { filter, map } from '@core/iterutil/pipe';
+import { pipe } from '@core/pipe';
+import sortOn from 'sort-on';
+import typia from 'typia';
+
+export const blogPosts = sortOn(
+	Array.from(
+		pipe(
+			Object.entries(import.meta.glob('$contents/blog/*.md', { eager: true })),
+
+			map(([filepath, md]) => ({
+				// eslint-disable-next-line ts/no-unsafe-member-access
+				...typia.assert<Metadata>((md as any).metadata),
+				filepath,
+			})),
+
+			/** filter out files without slug */
+			filter(({ slug }) => slug != null),
+
+			/** correct type */
+			map(({ slug, ...v }) => ({ ...v, slug })),
+
+			/** filter isPublished */
+			filter(({ isPublished }) => dev || isPublished),
+		),
+	),
+	['-pubDate'],
+) satisfies Item[];
--- blog/vite.config.ts
+++ blog/vite.config.ts
@@ -1,6 +1,10 @@
+import Typia from '@ryoppippi/unplugin-typia/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		Typia({}),
+		sveltekit(),
+	],
 });
