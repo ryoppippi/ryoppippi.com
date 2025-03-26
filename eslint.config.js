e25bb37fed96fe2fe126c9b747b2920194e7763c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-26 12:58:27 +0000

    feat: enable unocss eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { ryoppippi } from '@ryoppippi/eslint-config';
 
 export default ryoppippi({
 	type: 'app',
-	tailwind: false,
+	unocss: true,
 	svelte: true,
 	typescript: {
 		tsconfigPath: './tsconfig.json',
