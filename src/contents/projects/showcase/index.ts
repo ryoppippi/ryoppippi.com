b013254975b9f05a2a5cb0f049dc889d76b39930
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-21 19:12:20 +0100

    feat: use arktype in showcase

--- src/contents/projects/showcase/index.ts
+++ src/contents/projects/showcase/index.ts
@@ -1,28 +1,29 @@
 import type { MarkdownImport } from '../../../markdown';
 import { filter, flatten, map, reduce } from '@core/iterutil/pipe';
 import { pipe } from '@core/pipe';
+import { scope, type } from 'arktype';
 import sortOn from 'sort-on';
-import typia from 'typia';
 
-type Metadata = {
-	title: string;
-	link: string;
-	image: string;
-	pubDate: string;
-	featured?: boolean;
-};
-
-export type Project = Omit<Metadata, 'image' | 'featured'> & {
-	featured: boolean;
-	image: string;
-	Content: MarkdownImport<unknown>['default'];
-};
+export const { Project, Metadata } = scope({
+	Metadata: {
+		'title': 'string',
+		'slug': 'string',
+		'link': 'string',
+		'image': 'string',
+		'pubDate': 'string',
+		'featured?': 'boolean',
+	},
+	Project: {
+		'...': 'Required<Metadata>',
+		'Content': type('unknown').pipe(v => v as MarkdownImport<unknown>['default']),
+	},
+}).export();
 
 /** list of web projects */
-export function getProjects(): Project[] {
+export function getProjects(): (typeof Project.inferOut)[] {
 	const projectsIter = pipe(
 		/** import all markdown files in the directory */
-		Object.entries(import.meta.glob('./*.md', { eager: true })) as Iterable<[string, MarkdownImport<Metadata>]>,
+		Object.entries(import.meta.glob('./*.md', { eager: true })) as Iterable<[string, MarkdownImport<typeof Metadata.infer>]>,
 
 		/** get slug from the file path */
 		map(([filepath, md]) => {
@@ -37,20 +38,20 @@ export function getProjects(): Project[] {
 		filter(({ slug }) => slug != null),
 
 		/** filter with valid metadata */
-		filter(({ metadata }) => typia.is<Metadata>(metadata)),
+		filter(({ metadata }) => !(Metadata(metadata) instanceof type.errors)),
 
 		/** process each markdown file */
-		map(({ metadata, default: Content }) => ({
+		map(({ metadata, default: Content }) => (Project.assert({
 			...metadata,
 			featured: metadata?.featured ?? false,
 			Content,
-		} as const satisfies Project)),
+		}))),
 
 		/** separate featured and non-featured projects */
 		reduce((acc, project) => {
 			acc[project.featured ? 0 : 1].push(project);
 			return acc;
-		}, [[], []] as [Project[], Project[]]),
+		}, [[], []] as [typeof Project.inferOut[], typeof Project.inferOut[]]),
 
 		/** sort projects by pubDate */
 		map(projectGroups => sortOn(projectGroups, ['-pubDate'])),
