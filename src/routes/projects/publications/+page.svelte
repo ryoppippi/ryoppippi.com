41dd16efd821658cd53a93c19f4bc54674b62564
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-17 23:54:19 +0100

    fix: move publications to publication.json

--- src/routes/projects/publications/+page.svelte
+++ src/routes/projects/publications/+page.svelte
@@ -1,7 +1,7 @@
 <script lang='ts'>
 	import type { Item as ListItem } from '$components/ListView.svelte';
+	import publications from '$contents/publication.json';
 
-	type Year = `${number}`;
 	interface Item {
 		title: string;
 		link: string;
@@ -9,30 +9,6 @@
 		publisher: string;
 	}
 
-	const publications = {
-		2019: [
-			{
-				title: `Artificial Neural Networks for Realized Volatility Prediction in Cryptocurrency Time Series`, // spellchecker:disable-line
-				link: `https://link.springer.com/chapter/10.1007/978-3-030-22796-8_18`,
-				authors: `<span text-bold>Ryotaro Miura</span>, Lukáš Pichl, Taisei Kaizoji`,
-				publisher: `ISNN 2019: Advances in Neural Networks 2019`,
-			},
-			{
-				title: `An Intuitive Interface for Digital Synthesizer by Pseudo-intention Learning`,
-				link: `https://dl.acm.org/doi/10.1145/3356590.3356598`,
-				authors: `Keisuke Shiro, Ryotaro Miura, Changyo Han and Jun Rekimoto`,
-				publisher: `Audio Mostly 2019`,
-			},
-		],
-		2022: [
-			{
-				title: `Prometheus: A mobile telepresence system connecting the 1st person and 3rd person perspectives continuously`,
-				link: `https://dl.acm.org/doi/10.1145/3550082.3564187`,
-				authors: `Ryotaro Kimura, Jun Rekimoto`,
-				publisher: `SIGGRAPH Asia 2022 `,
-			},
-		],
-	} as const satisfies Record<Year, Item[]>;
 </script>
 
 {#snippet itemView(_item: ListItem)}
