dc6f4bec71810e6311e7e22615b09c254d2e20a5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-29 00:52:22 +0100

    feat: pass github token by .env

--- src/lib/server/octokit.ts
+++ src/lib/server/octokit.ts
@@ -1,12 +1,10 @@
-import { building } from '$app/environment';
-import { env } from '$env/dynamic/private';
+import { GITHUB_TOKEN } from '$env/static/private';
 import { Octokit } from 'octokit';
 
 let octokit: Octokit | undefined;
 
 export function useOctokit(): Octokit {
 	return octokit ?? new Octokit({
-		/* you can add optional auth here */
-		auth: !building ? env.GITHUB_TOKEN : undefined,
+		auth: GITHUB_TOKEN as string ?? undefined,
 	});
 }
