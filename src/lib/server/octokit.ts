2dd11404eb35c96ce2f607595d79ecf5f2bb0dc5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-29 01:02:52 +0100

    Revert "feat: pass github token by .env"
    This reverts commit dc6f4bec71810e6311e7e22615b09c254d2e20a5.

--- src/lib/server/octokit.ts
+++ src/lib/server/octokit.ts
@@ -1,10 +1,12 @@
-import { GITHUB_TOKEN } from '$env/static/private';
+import { building } from '$app/environment';
+import { env } from '$env/dynamic/private';
 import { Octokit } from 'octokit';
 
 let octokit: Octokit | undefined;
 
 export function useOctokit(): Octokit {
 	return octokit ?? new Octokit({
-		auth: GITHUB_TOKEN as string ?? undefined,
+		/* you can add optional auth here */
+		auth: !building ? env.GITHUB_TOKEN : undefined,
 	});
 }
