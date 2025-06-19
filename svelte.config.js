72696e7cdd28ea6e6cf406bd2f11b1adbee7a567
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-06-19 18:35:20 +0100

    fix: assets path

--- svelte.config.js
+++ svelte.config.js
@@ -160,15 +160,7 @@ const config = {
 			})(),
 		},
 		paths: {
-			/**
-			  @see https://developers.cloudflare.com/pages/configuration/build-configuration#environment-variables
-			  @see https://kit.svelte.jp/docs/configuration#paths
-			 */
-			assets: (isDevelopment
-				? ''
-				: process.env.CF_PAGES_BRANCH !== 'main'
-					? process.env.CF_PAGES_URL
-					: undefined) ?? 'https://ryoppippi.com',
+			assets: (isDevelopment ? '' : 'https://ryoppippi.com'),
 		},
 	},
 };
