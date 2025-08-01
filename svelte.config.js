8d77a32ed7ace01a82175d9b5ad7d5f7c0993f84
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-08-01 22:11:38 +0100

    fix: stop using async for now
    https://github.com/sveltejs/svelte/issues/16429#issuecomment-3089268214

--- svelte.config.js
+++ svelte.config.js
@@ -117,12 +117,6 @@ const config = {
 		},
 	},
 
-	compilerOptions: {
-		experimental: {
-			async: true,
-		},
-	},
-
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
