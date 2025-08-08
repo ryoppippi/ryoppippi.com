4ac8ba74fdcc114e8f9ce456d1b374e0579e43b6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-08-08 17:22:10 +0100

    refactor(GitHubNav): replace ufo.joinURL with native URL API
    - Remove ufo import
    
    - Use new URL().toString() for link construction

--- src/components/GitHubNav.svelte
+++ src/components/GitHubNav.svelte
@@ -1,6 +1,5 @@
 <script lang='ts'>
 	import { PUBLIC_ORIGIN } from '$env/static/public';
-	import * as ufo from 'ufo';
 </script>
 
 {#snippet link(
@@ -14,7 +13,7 @@
 		fcol-md-row
 		fyc
 		gap-1
-		href={ufo.joinURL(PUBLIC_ORIGIN, _subdomain)}
+		href={new URL(_subdomain, PUBLIC_ORIGIN).toString()}
 		target='_blank'
 	>
 		<!-- svelte-ignore element_invalid_self_closing_tag -->
