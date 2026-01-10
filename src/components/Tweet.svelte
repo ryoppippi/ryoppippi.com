ba969eac5e362d8f6311eac603c814ddd4f3f555
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-01-10 22:44:52 +0000

    Revert "feat: use hydratable for getting tweet"
    This reverts commit 86bffb468ff881f2ed26233c43551eb376352d63.

--- src/components/Tweet.svelte
+++ src/components/Tweet.svelte
@@ -1,17 +1,10 @@
 <script lang='ts'>
-	import { hydratable } from 'svelte';
+	import { getTweet } from '$lib/tweet.remote.js';
 	import * as st from 'sveltweet';
-	import * as stApi from 'sveltweet/api';
 
-	type Props = {
-		id: string;
-	};
+	type Props = Parameters<typeof getTweet>[0];
 
 	const { id }: Props = $props();
-
-	const tweet = await hydratable(`tweet-${id}`, async () => {
-		return stApi.getTweet(id);
-	});
 </script>
 
-<st.Tweet {tweet} />
+<st.Tweet tweet={await getTweet({ id })} />
