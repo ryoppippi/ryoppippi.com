284462e53f731762cab274c8efecc3fc48d07bd8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-08-31 14:19:22 +0100

    chore(nix): silence agent skills sync output

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@
                   fi
                 fi
 
-                ${nixpkgs.lib.getExe agentSkills.syncAgentSkills}
+                ${nixpkgs.lib.getExe agentSkills.syncAgentSkills} >/dev/null
               '';
             };
         }
